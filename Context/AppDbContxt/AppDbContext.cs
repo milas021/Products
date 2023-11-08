025eb1c3d2f284e7f2a1d5b42b6dafab89722a68
milad <milad@MILAS>
2023-11-08 22:53:44 +0330

    Add project files.

---
+++ Context/AppDbContxt/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Products.Model;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Products.Context.AppDbContxt
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product>   Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;Database=Shop;User Id=sa;Password=123456;");
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Products.Context.AppDbContxt;
+using Products.Model;
+
+namespace Products.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpPost("product")]
+        public async Task<IActionResult> Get()
+        {
+            var db = new AppDbContext();
+            var result = db.Products.ToList();
+
+            return Ok(result);
+        }
+    }
+}
